e187657ea553ace0baf0e524820b592fa2dafc1f
Matthew Dillon <mddillon47@gmail.com>
2022-02-17 00:52:43 -0800

    codesphere interview

---
+++ index.js
@@ -0,0 +1,32 @@
+function combineImgs(img1, img2) {
+    let finalImg = [];
+    let subArr = [];
+
+    [black, white, white, [white, black, white, black]]
+
+
+    for (let i = 0; i < img1.length; i++) {
+        
+        if (typeof block === array) {
+            finalImg[i] = combineImgs(block, img2[i]);
+        }
+            
+        }
+
+        if (block === "black") {
+            finalImg[i] = "black"
+        }
+
+        if (block === 'white') {
+            if (img2[i] === 'white') {
+                finalImg[i] = 'white'
+            } else if (img2[i] === 'black'){
+                finalImg[i] = 'black'
+            }
+            
+        }
+        
+    }
+
+    return finalImg;
+}
